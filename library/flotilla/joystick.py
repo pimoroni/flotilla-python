6cda66974b4c8bb0c06bbd5fa4c28409643963f0
RogueM <hal@pimoroni.com>
2017-05-23 19:11:15 +0000

    iverted axis direction + new joystick rotation method

--- library/flotilla/joystick.py
+++ library/flotilla/joystick.py
@@ -1,8 +1,21 @@
 from .module import Module
 
+_xstart = 1023
+_ystart = 1023
 
 class Joystick(Module):
     name = 'joystick'
+
+    def rotation(self, r=0):
+        global _rotation
+        global _xstart
+        global _ystart
+        if r == 0:
+            _xstart = 1023
+            _ystart = 1023
+        elif r == 180:
+            _xstart = 0
+            _ystart = 0
 
     @property
     def button(self):
@@ -13,11 +26,11 @@ class Joystick(Module):
     @property
     def x(self):
         if len(self.data) > 1:
-            return int(self.data[1])
+            return abs(_xstart - int(self.data[1]))
         return 0
 
     @property
     def y(self):
         if len(self.data) > 2:
-            return int(self.data[2])
+            return abs(_ystart - int(self.data[2]))
         return 0
