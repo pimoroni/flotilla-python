eb134f14a27de6190ab322522ec2e772f4ec81cd
Philip Howard <phil@gadgetoid.com>
2017-08-22 10:53:31 +0100

    Added Magnetometer values

--- library/flotilla/motion.py
+++ library/flotilla/motion.py
@@ -21,3 +21,21 @@ class Motion(Module):
         if len(self.data) > 2:
             return int(self.data[2])
         return 0
+
+    @property
+    def magnetometer_x(self):
+        if len(self.data) > 3:
+            return int(self.data[3])
+        return 0
+
+    @property
+    def magnetometer_y(self):
+        if len(self.data) > 4:
+            return int(self.data[4])
+        return 0
+
+    @property
+    def magnetometer_z(self):
+        if len(self.data) > 5:
+            return int(self.data[5])
+        return 0
